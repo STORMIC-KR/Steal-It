f0eec47bef64c948606e6e08d8a411076dcaf6a6
STORMIC-KR <53865298+STORMIC-KR@users.noreply.github.com>
2021-08-28 13:59:09 +0900

    ..

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,80 +4,32 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public Rigidbody rb;
+    public float maxTorque = 50f;
+    public float steerForce = 2f;
 
-    public float forwardAccel = 8f, reverseAccel = 8f, maxSpeed = 50f, turnStrength = 180f, gravityForce = 10f, dragOnGround = 3f;
+    public WheelCollider[] wheelColliders = new WheelCollider[4];
+    public Transform[] tireMeshes = new Transform[4];
 
-    private float speedInput, turnInput;
-
-    private bool grounded;
-
-    public LayerMask whatIsGround;
-    public float groundRayLength = 0.5f;
-    public Transform groundRayPoint;
-
-    public Transform leftFrontWheel, rightFrontWheel;
-    public float maxWheelTurn = 25f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb.transform.parent = null;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        speedInput = 0f;
-        if(Input.GetAxis("Vertical") > 0)
-        {
-            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
-        }
-        else if(Input.GetAxis("Vertical") < 0)
-        {
-            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
-        }
-
-        turnInput = Input.GetAxis("Horizontal");
-
-        if (grounded)
-        {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
-        }
-
-        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, leftFrontWheel.localRotation.eulerAngles.z);
-        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, turnInput * maxWheelTurn, rightFrontWheel.localRotation.eulerAngles.z);
-
-        transform.position = rb.transform.position;
+        UpdateMeshesPositions();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        grounded = false;
-        RaycastHit hit;
+        
+    }
 
-        if(Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+    void UpdateMeshesPositions()
+    {
+        for(int i = 0; i < 4; i++)
         {
-            grounded = true;
+            Quaternion quat;
+            Vector3 pos;
+            wheelColliders[i].GetWorldPose(out pos, out quat);
 
-            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-        }
-
-        if (grounded)
-        {
-            rb.drag = dragOnGround;
-
-
-            if (Mathf.Abs(speedInput) > 0)
-            {
-                rb.AddForce(transform.forward * speedInput);
-            }
-        }
-        else
-        {
-            rb.drag = 0.1f;
-
-            rb.AddForce(Vector3.up * -gravityForce * 100f);
+            tireMeshes[i].position = pos;
+            tireMeshes[i].rotation = quat;
         }
     }
 }
